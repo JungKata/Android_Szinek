c7b93b52085ba961a29288b98062f8d618468d6e
JungKata <97729333+JungKata@users.noreply.github.com>
2022-09-16 09:30:13 +0200

    fekete háttér piros szöveg szinek beallitasa

--- app/src/main/java/com/example/sznek/MainActivity.java
+++ app/src/main/java/com/example/sznek/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.sznek;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private RelativeLayout layout;
+    private TextView textSzin;
+    private Random rnd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+        layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int red = rnd.nextInt(256);
+                int green = rnd.nextInt(256);
+                int blue = rnd. nextInt(256);
+                layout.setBackgroundColor(Color.rgb(red, green, blue));
+            }
+        });
+    }
+    private void init()
+    {
+        layout = findViewById(R.id.layout);
+        textSzin = findViewById(R.id.textSzin);
+        rnd = new Random();
     }
 }
